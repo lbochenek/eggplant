aab176cdd54c61b1ef1c4672ba82902add6bdd31
Laura Bochenek <laura.bochenek@gmail.com>
2015-07-07 13:43:24 -0400

    converts emoji between surrogate pair and code point

--- interpretter.js
+++ interpretter.js
@@ -1,15 +1,59 @@
 //http://scratch-lang.notimetoplay.org/index.html
 
-//supplies the next word of input on demand
-function lexer(text)
+window.onload = function() {
+  document.querySelector("#process").addEventListener("click", eggplant);
+};
+
+function eggplant()
 {
-  var words = text.split(/\s+/);
-  var next = 0;
-  this.nextWord = function(){
-    if(next >= words.length)
-      return null;
-    var ret = words[next];
-    next++;
-    return ret;
+  var words = getText();
+  console.log("code point: " + toCodePoint(words));
+  console.log("unicode surrogate: " + fromCodePoint(toCodePoint(words)));
+}
+
+//http://www.christinahsuholland.com/processing-emojis-in-javascript/
+//https://github.com/twitter/twemoji
+function toCodePoint(unicodeSurrogates, sep)
+{
+  var r=[]; //holds numbers
+  var c=0; //char code at specific index
+  var p=0; //if within range (maybe BPM?)
+  var i=0; //index
+  while(i < unicodeSurrogates.length)
+  {
+    c = unicodeSurrogates.charCodeAt(i);
+    i++;
+    if(p)
+    {
+      r.push((0x10000 + ((p-0xD800)<<10)+(c-0xDC00)).toString(16));
+      p = 0;
+    }
+    else if(0xD800 <= c && c<=0xDBFF)
+    {
+      p = c;
+    }
+    else
+    {
+      r.push(c.toString(16));
+    }
   }
+  return r.join(sep || '-');
+}
+
+//http://www.christinahsuholland.com/processing-emojis-in-javascript/
+//https://github.com/twitter/twemoji
+function fromCodePoint(codepoint)
+{
+  var code = typeof codepoint === 'string'? parseInt(codepoint, 16):codepoint;
+  if(code < 0x10000)
+  {
+    return String.fromCharCode(code);
+  }
+  code -= 0x10000;
+  return String.fromCharCode(0xD800 + (code >> 10), 0xDC00 + (code & 0x3FF));
+}
+
+function getText()
+{
+  return document.querySelector("#language").value;
 }
